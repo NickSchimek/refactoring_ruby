b0a176093418d1b01ec3c6b0700c8da15a4b2068
Nicholas Schimek <cfme00@gmail.com>
2018-08-16 18:04:23 +0000

    Add tests for new release

--- spec/customer_spec.rb
+++ spec/customer_spec.rb
@@ -1,4 +1,6 @@
 require 'customer'
+require 'movie'
+require 'rental'
 
 RSpec.describe Customer do
 
@@ -6,4 +8,25 @@ RSpec.describe Customer do
     customer = Customer.new "Cess"
     expect(customer.name).to eq("Cess")
   end
+  
+  it "prints statement when customer has no rentals" do
+    customer = Customer.new "Nick"
+    expect(customer.statement).to eq("Rental Record for Nick\nAmount owed is 0\nYou earned 0 frequent renter points")
+  end
+  
+  it "prints statement when customer has one new release" do
+    customer = Customer.new "New release"
+    new_release = Movie.new "Terminator", Movie::NEW_RELEASE
+    rental = Rental.new new_release, 1
+    customer.add_rental rental
+    expect(customer.statement).to eq("Rental Record for New release\n\tTerminator\t3\nAmount owed is 3\nYou earned 1 frequent renter points")
+  end
+  
+  it "prints statement when customer has one new release" do
+    customer = Customer.new "New release"
+    new_release = Movie.new "Terminator", Movie::NEW_RELEASE
+    rental = Rental.new new_release, 2
+    customer.add_rental rental
+    expect(customer.statement).to eq("Rental Record for New release\n\tTerminator\t6\nAmount owed is 6\nYou earned 2 frequent renter points")
+  end  
 end
